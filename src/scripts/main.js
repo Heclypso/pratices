9788e4929b30a3b2e61a78d217d1bdb6edc62404
Heclypso <heclypso@gmail.com>
2025-03-24 16:15:12 -0300

    #2 feat: desenvolvido a função de comunicação com a api

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,3 @@
-import {somar} from './package.js'
+import {chamarApi} from './package.js'
 
-const button = document.getElementById('btn-calc')
-
-button.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    const firstNumber = document.getElementById('first-number').value
-    const secondNumber = document.getElementById('second-number').value
-
-    console.log(firstNumber)
-    console.log(secondNumber)
-
-    try {
-        console.log(somar(Number(firstNumber), Number(secondNumber)))
-    } catch(e) {
-        console.log("Ocorreu um erro", e)
-    }
-})
+chamarApi() // chamada da função que está sendo exportada de package.js
--- src/scripts/package.js
+++ src/scripts/package.js
@@ -1,1 +1,16 @@
-export const somar = (a, b) =>  a + b;
+const endpoint = 'https://dummyjson.com/products'; // url da requisição ao servidor
+
+export const chamarApi = async () => { // convertido a função chamarApi para arrow function
+    try { // a função vai tentar executar esse bloco
+        const resp = await fetch(endpoint)
+
+        if (resp.status === 200) {
+            const obj = await resp.json();
+            console.log(obj)
+        }
+
+    } catch (error) { // se o bloco anterior (try) falhar esse bloco será executado
+        console.log('Ocorreu um erro', error)
+    }
+}
+

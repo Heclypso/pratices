bd6223c2f24c023972cf237e886bd937b0416b02
Heclypso <heclypso@gmail.com>
2025-03-28 16:56:34 -0300

    #9 feat: desenvolvido um adress para a propriedade motor do carroDaJulia e praticado spread

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,3 +8,20 @@ button.addEventListener('click', e => {
 })
 
 console.log(somarComRest(20,50,30))
+
+const carroDaAna = {
+    modelo: 'ka',
+    fabricante: 'ford',
+    motor: 1.8
+}
+
+const carroDaJulia = {
+    ...carroDaAna,
+    motor: 2.0
+}
+
+carroDaJulia.motor 
+
+const { motor: motorDoCarroDaJulia } = carroDaJulia;
+
+console.log(motorDoCarroDaJulia)
